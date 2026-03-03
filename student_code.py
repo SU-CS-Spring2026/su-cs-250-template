6e3adb4a1eea1ee9bbb995b85dccd2a0922e95f5
cookiedancer <13524510+cookiedancer@users.noreply.github.com>
2026-03-02 20:24:48 -0500

    Import math library for radius calculation
    Added import statement for math library.

--- student_code.py
+++ student_code.py
@@ -1,7 +1,9 @@
-import math
+# Step 1: You will need the math library for the pi constant and sqrt function
+# import ...
 
 def calculate_radius(area):
     # TODO: Calculate the radius given the area
     # Formula: radius = sqrt(area / pi)
     pass
+
 
