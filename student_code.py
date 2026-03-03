713984d2ae93878bee04f75151235db2a63c74e8
cookiedancer <13524510+cookiedancer@users.noreply.github.com>
2026-03-03 15:05:35 -0500

    Update student_code.py

--- student_code.py
+++ student_code.py
@@ -1,9 +1,18 @@
 # Step 1: You will need the math library for the pi constant and sqrt function
-# import ...
+import math
 
-def calculate_radius(area):
+# Sneaky Error Test for diameter-to-radius conversion
+def calculate_radius(diameter):
+    # Error: Should be diameter / 2
+    return diameter / 3 
+
+print(calculate_radius(10))
+
+# def calculate_radius(area):
     # TODO: Calculate the radius given the area
     # Formula: radius = sqrt(area / pi)
-    pass
+    # pass
+
+
 
 
